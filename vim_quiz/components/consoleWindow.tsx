f607ee7199f878e6994b33b1ad01a839bd3e3bff
Daianywhere <qwerty190854@goo.jp>
2022-11-30 00:33:19 +0900

    toConsoleTextをクラス内部に移動

--- vim_quiz/components/consoleWindow.tsx
+++ vim_quiz/components/consoleWindow.tsx
@@ -6,40 +6,40 @@ type textObj = {
   text: string
 };
 
-function toConsoleText(textObjs: textObj[]) {
-  let ret = Array();
-
-  /**
-   * アニメーションコマ数を取得する
-   */
-  const animationNum = Math.max(
-    ...textObjs.map(
-      singleObj => singleObj.animationNum ? singleObj.animationNum : 1
-    )
-  );
-
-  textObjs.forEach((singleObj, index) => {
-    const animationSteps =
-      animationNum == 1 ? 'always'
-      : `${singleObj.animationNum ? singleObj.animationNum : 1}of${animationNum}`;
-    switch (singleObj.style) {
-      case 'blink':
-        ret.push((<span key={index} className={`char-pointer-${animationSteps}`}>{singleObj.text}</span>))
-        break;
-      case 'none':
-      default:
-        ret.push((<span key={index}>{singleObj.text}</span>))
-        break;
-    }
-  })
-
-  return ret;
-}
-
 class ConsoleWindow extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {textElems: toConsoleText(this.props.text)}
+    this.state = {textElems: this.toConsoleText(this.props.text)}
+  }
+
+  toConsoleText(textObjs: textObj[]) {
+    let ret = Array();
+
+    /**
+     * アニメーションコマ数を取得する
+     */
+    const animationNum = Math.max(
+      ...textObjs.map(
+        singleObj => singleObj.animationNum ? singleObj.animationNum : 1
+      )
+    );
+
+    textObjs.forEach((singleObj, index) => {
+      const animationSteps =
+        animationNum == 1 ? 'always'
+        : `${singleObj.animationNum ? singleObj.animationNum : 1}of${animationNum}`;
+      switch (singleObj.style) {
+        case 'blink':
+          ret.push((<span key={index} className={`char-pointer-${animationSteps}`}>{singleObj.text}</span>))
+          break;
+        case 'none':
+        default:
+          ret.push((<span key={index}>{singleObj.text}</span>))
+          break;
+      }
+    })
+
+    return ret;
   }
 
   render () {
