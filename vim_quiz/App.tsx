ee3e0c5a211db78e50bd933469408e6f8517694d
Daianywhere <qwerty190854@goo.jp>
2022-11-29 22:38:09 +0900

    アニメーション設定

--- vim_quiz/App.tsx
+++ vim_quiz/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <link rel={'stylesheet'} href={'./static/css/App.css'}/>
       <div className="App">
-        <ConsoleWindow text={[{style: 'blink-always', text: 'H'}, {text: 'ello, world'}]}/>
+        <ConsoleWindow text={[{style: 'blink', animationNum:1, text: 'H'}, {text: 'ello, world'}, {style: 'blink', animationNum:2, text:'!'}]}/>
       </div>
     </>
   );
--- vim_quiz/components/consoleWindow.tsx
+++ vim_quiz/components/consoleWindow.tsx
@@ -2,16 +2,29 @@ import { React } from "../deps.ts";
 
 type textObj = {
   style?: string,
+  animationNum?: number,
   text: string
 };
 
 function toConsoleText(textObjs: textObj[]) {
   let ret = Array();
 
+  /**
+   * アニメーションコマ数を取得する
+   */
+  const animationNum = Math.max(
+    ...textObjs.map(
+      singleObj => singleObj.animationNum ? singleObj.animationNum : 1
+    )
+  );
+
   textObjs.forEach((singleObj, index) => {
+    const animationSteps =
+      animationNum == 1 ? 'always'
+      : `${singleObj.animationNum ? singleObj.animationNum : 1}of${animationNum}`;
     switch (singleObj.style) {
-      case 'blink-always':
-        ret.push((<span key={index} className="char-pointer-always">{singleObj.text}</span>))
+      case 'blink':
+        ret.push((<span key={index} className={`char-pointer-${animationSteps}`}>{singleObj.text}</span>))
         break;
       case 'none':
       default:
