b8f0014cd11ad23c4310cd35b26a989da4467874
Daianywhere <qwerty190854@goo.jp>
2022-11-29 23:05:11 +0900

    Appをクラス化

--- vim_quiz/App.tsx
+++ vim_quiz/App.tsx
@@ -1,15 +1,21 @@
 import {React} from './deps.ts';
 import ConsoleWindow from './components/consoleWindow.tsx'
 
-function App() {
-  return (
-    <>
-      <link rel={'stylesheet'} href={'./static/css/App.css'}/>
-      <div className="App">
-        <ConsoleWindow text={[{style: 'blink', animationNum:1, text: 'H'}, {text: 'ello, world'}, {style: 'blink', animationNum:2, text:'!'}]}/>
-      </div>
-    </>
-  );
+class App extends React.Component {
+  constructor () {
+    super();
+  }
+
+  render() {
+    return (
+      <>
+        <link rel={'stylesheet'} href={'./static/css/App.css'}/>
+        <div className="App">
+          <ConsoleWindow text={[{style: 'blink', animationNum:1, text: 'H'}, {text: 'ello, world'}, {style: 'blink', animationNum:2, text:'!'}]}/>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
