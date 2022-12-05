b86ce3e33f8ec3b31fd434c5a5aa68d3c9ceb63c
Daianywhere <qwerty190854@goo.jp>
2022-12-05 19:36:26 +0900

    keyConvを作成

--- vim_quiz/functions/keyHandler.ts
+++ vim_quiz/functions/keyHandler.ts
@@ -1,9 +1,19 @@
+const keyConv = (key: string): string => {
+  switch (key) {
+    case 'Backspace':
+      return 'BS';
+    case 'Escape':
+      return 'Esc';
+  }
+  return key;
+};
+
 function* keyHandler() {
   let stack: string[] = [];
   let input: string = '';
   while (true) {
     input = yield stack;
-    stack.push(input);
+    stack.push(keyConv(input));
   }
 }
 
