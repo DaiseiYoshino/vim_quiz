d2f8c3b880f2107fc4c7f45967d70ee8fc34b3ae
Daisei Yoshino <daisei.yoshino@sencorp.co.jp>
2023-07-07 13:24:23 +0900

    構成を変更

--- vim_quiz/components/consoleWindow.tsx
+++ vim_quiz/components/consoleWindow.tsx
@@ -14,29 +14,13 @@ class ConsoleWindow extends React.Component {
   toConsoleText(textObjs: Array<string | textObj>) {
     let ret = Array();
 
-    /**
-     * アニメーションコマ数を取得する
-     */
-    const animationNum: number = Math.max(
-      ...textObjs.map(
-        singleObj => {
-          if (typeof singleObj == 'string') return 1;
-          return singleObj?.animationNum ? singleObj.animationNum : 1
-        }
-      )
-    );
-
     textObjs.forEach((singleObj, index) => {
-      const animationSteps =
-        animationNum == 1 ? 'always'
-          : typeof singleObj == 'string' ? 'always' 
-          : `${singleObj.animationNum ? singleObj.animationNum : 1}of${animationNum}`;
       if (typeof singleObj == 'string') {
         ret.push((<span key={index}>{singleObj}</span>))
       } else {
         switch (singleObj.style) {
           case 'blink':
-            ret.push((<span key={index} className={`char-pointer-${animationSteps}`}>{singleObj.text}</span>))
+            ret.push((<span key={index} className={`char-pointer-always`}>{singleObj.text}</span>))
             break;
           case 'none':
           default:
